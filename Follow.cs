599ed27cbc680129670c2e9db2c983381087c879
Ivo Potato <64116534+DrIvoPotato@users.noreply.github.com>
2020-04-22 05:08:45 +0200

    Add files via upload

---
+++ Follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Follow : MonoBehaviour
+{
+    public Transform Player; //Change Player to a name of your main actor
+    public Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.position + offset; //Change Player to a name of your main actor
+    }
+}
