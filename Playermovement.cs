d9cd42b386f85ef609394869be3dd2017a089f66
Ivo Potato <64116534+DrIvoPotato@users.noreply.github.com>
2020-04-22 05:13:07 +0200

    Movement.Constantly forward + Sideways with keys

---
+++ Playermovement.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public float sideways = 500f;
+    public Rigidbody rb;
+    public float forwardforce = 2000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+
+
+
+
+
+
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideways * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+        
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideways * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+
+
+        }
+
+
+
+
+
+    }
+
+}
